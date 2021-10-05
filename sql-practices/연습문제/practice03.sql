dc08c3ece601c55cfbf757770d0e83dbf644fb65
jkl851 <jkl851@naver.com>
2021-10-05 21:50:56 +0900

    20211005 sql practice04 문제 3까지 완료

--- sql-practices/연습문제/practice03.sql
+++ sql-practices/연습문제/practice03.sql
@@ -4,6 +4,7 @@
 select E.emp_no, E.first_name, S.salary
 	from employees E left join salaries S on E.emp_no = S.emp_no left join titles T on E.emp_no = T.emp_no
     where T.to_date = '9999-01-01'
+    and S.to_date = '9999-01-01'
     group by E.emp_no
     order by S.salary desc;
 
@@ -11,36 +12,92 @@ select E.emp_no, E.first_name, S.salary
 -- 전체 사원의 사번, 이름, 현재 직책을 이름 순서로 출력하세요.
 select E.emp_no, E.first_name, T.title
 	from employees E left join titles T on E.emp_no = T.emp_no 
+    where T.to_date = '9999-01-01'
     order by E.first_name asc;
 
 -- 문제3.
 -- 전체 사원의 사번, 이름, 현재 부서를 이름 순서로 출력하세요..
--- select E.emp_no, E.first_name, DE.dept_no
--- 	from employees E left join dept_emp DE on E.emp_no = DE.emp_no, departments DP 
---     group by E.first_name
--- 	order by E.first_name asc;
+select E.emp_no, E.first_name, DP.dept_name
+	from employees E left join dept_emp DE on E.emp_no = DE.emp_no join departments DP on DE.dept_no = DP.dept_no 
+	where DE.to_date = '9999-01-01'
+    group by E.first_name
+	order by E.first_name;
 
 
 -- 문제4.
 -- 전체 사원의 사번, 이름, 연봉, 직책, 부서를 모두 이름 순서로 출력합니다.
+select E.emp_no, E.first_name, SR.salary, TT.title, DP.dept_name
+	from employees E left join dept_emp DE on E.emp_no = DE.emp_no 
+		join departments DP on DE.dept_no = DP.dept_no 
+        join titles TT on E.emp_no = TT.emp_no
+        join salaries SR on E.emp_no = SR.emp_no
+    group by E.first_name
+	order by E.first_name;
+	
 
 -- 문제5.
 -- ‘Technique Leader’의 직책으로 과거에 근무한 적이 있는 모든 사원의 사번과 이름을 출력하세요. 
 -- (현재 ‘Technique Leader’의 직책(으로 근무하는 사원은 고려하지 않습니다.) 
 -- 이름은 first_name과 last_name을 합쳐 출력 합니다.
+select E.emp_no, concat(E.first_name, ' ', E.last_name) as full_name, TT.title
+	from employees E left join dept_emp DE on E.emp_no = DE.emp_no 
+        join titles TT on E.emp_no = TT.emp_no
+	where TT.title = 'Technique Leader'
+    and TT.to_date != '9999-01-01'
+    and DE.to_date != '9999-01-01'
+    order by full_name;
 
 -- 문제6.
 -- 직원 이름(last_name) 중에서 S(대문자)로 시작하는 직원들의 이름, 부서명, 직책을 조회하세요.
+select concat(E.first_name, ' ', E.last_name) as full_name, DP.dept_name, TT.title
+	from employees E left join dept_emp DE on E.emp_no = DE.emp_no 
+        join titles TT on E.emp_no = TT.emp_no
+        join departments DP on DE.dept_no = DP.dept_no
+	where substring(E.last_name, 1, 1) = upper('s')
+    order by full_name;
+
 
 -- 문제7.
 -- 현재, 직책이 Engineer인 사원 중에서 현재 급여가 40000 이상인 사원을 급여가 큰 순서대로 출력하세요.
+select *
+	from employees E left join titles TT on E.emp_no = TT.emp_no
+        join salaries SR on E.emp_no = SR.emp_no
+	where TT.title = 'Engineer'
+    and SR.salary >= 40000
+    and SR.to_date = '9999-01-01'
+    and TT.to_date = '9999-01-01'
+	order by SR.salary desc;
+
 
 -- 문제8.
--- 현재 급여가 50000이 넘는 직책을 직책, 급여로 급여가 큰 순서대로 출력하시오
+-- 현재 급여가 50000이 넘는 직원을 직책, 급여로 급여가 큰 순서대로 출력하시오
+select E.emp_no, TT.title, SR.salary
+	from employees E left join titles TT on E.emp_no = TT.emp_no
+        join salaries SR on E.emp_no = SR.emp_no
+	where SR.salary > 50000
+    and SR.to_date = '9999-01-01'
+    and TT.to_date = '9999-01-01'
+	order by SR.salary desc;
 
 -- 문제9.
 -- 현재, 부서별 평균 연봉을 연봉이 큰 부서 순서대로 출력하세요.
+select DP.dept_name, avg(SR.salary) as avg_salary
+	from employees E left join dept_emp DE on E.emp_no = DE.emp_no 
+		join departments DP on DE.dept_no = DP.dept_no 
+        join salaries SR on E.emp_no = SR.emp_no
+	where SR.to_date = '9999-01-01'
+    and DE.to_date = '9999-01-01'
+    group by DP.dept_name
+	order by avg_salary desc;
+
 
 -- 문제10.
 -- 현재, 직책별 평균 연봉을 연봉이 큰 직책 순서대로 출력하세요.
+select TT.title, avg(SR.salary) as avg_salary
+	from employees E left join titles TT on E.emp_no = TT.emp_no
+        join salaries SR on E.emp_no = SR.emp_no
+	where SR.to_date = '9999-01-01'
+    and TT.to_date = '9999-01-01'
+	group by TT.title
+	order by avg_salary desc;
  
--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -1,25 +1,62 @@
 -- practice04 서브쿼리 혼합
 -- 문제1.
 -- 현재 평균 연봉보다 많은 월급을 받는 직원은 몇 명이나 있습니까?
-
+select count(E.emp_no)
+	from employees E left join salaries SR on E.emp_no = SR.emp_no
+	where SR.to_date = '9999-01-01'
+	and SR.salary > (select avg(salary) from salaries where to_date = '9999-01-01');
+    
 -- 문제2. 
 -- 현재, 각 부서별로 최고의 급여를 받는 사원의 사번, 이름, 부서 연봉을 조회하세요. 
 -- 단 조회결과는 연봉의 내림차순으로 정렬되어 나타나야 합니다. 
+select E.emp_no, E.first_name, DP.dept_name, SR.salary
+	from employees E join salaries SR on E.emp_no = SR.emp_no
+		join dept_emp DE on E.emp_no = DE.emp_no 
+		join departments DP on DE.dept_no = DP.dept_no 
+        join (select DE.dept_no, max(salary) as max_salary from salaries SR 
+									join dept_emp DE on SR.emp_no = DE.emp_no 
+									where SR.to_date = '9999-01-01' 
+                                    and DE.to_date = '9999-01-01' 
+                                    group by DE.dept_no) d on DE.dept_no = d.dept_no 
+	where SR.to_date = '9999-01-01'
+	and DE.to_date = '9999-01-01'
+	and SR.salary = d.max_salary
+    order by SR.salary desc;
+
 
 -- 문제3.
 -- 현재, 자신의 부서 평균 급여보다 연봉(salary)이 많은 사원의 사번, 이름과 연봉을 조회하세요 
+select a.emp_no, a.first_name, b.salary
+	from employees a, salaries b, dept_emp c, (select a.dept_no, avg(salary) as avg_salary
+									from dept_emp a, salaries b, departments c
+									where a.emp_no = b.emp_no
+									and a.dept_no = c.dept_no
+									and b.to_date = '9999-01-01'
+									and a.to_date = '9999-01-01'
+									group by c.dept_name) d
+    where a.emp_no = b.emp_no
+    and a.emp_no = c.emp_no
+    and c.dept_no = d.dept_no
+    and b.to_date = '9999-01-01'
+	and c.to_date = '9999-01-01'
+    and b.salary > d.avg_salary;
+
 
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
 
+
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
 
+
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
